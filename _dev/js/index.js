bb5b333bdefd900157d16d378a67a5e3c60068d1
VPayonk <Qwerty123!>
2020-06-25 18:46:14 +0300

    4comm

--- _dev/js/index.js
+++ _dev/js/index.js
@@ -1,3 +1,15 @@
+let foopicker = new FooPicker({
+    id: 'datepicker',
+    dateFormat: 'dd/MM/yyyy'
+});
 
+$('.btn-date').on('click', function () {
+    let datepicker = $('#datepicker');
+    let date = new Date()
+    let day = date.getDate()
+    let month = date.getMonth() < 9 ? `0${date.getMonth() + 1}` : date.getMonth() + 1
+    let year = date.getFullYear()
 
+    datepicker.val(`${day}/${month}/${year}`)
+})
 
