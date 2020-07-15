61f53b07344472ac667867760e79ce2c54de87c5
VPayonk <Qwerty123!>
2020-07-15 18:31:30 +0300

    8comm

--- _dev/js/clinic.js
+++ _dev/js/clinic.js
@@ -1,2 +1,58 @@
 
+$('.play-btn').on('click', function() {
+    initVideo()
+})
+
+
+
+function initVideo() {
+    var tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    var player;
+
+
+
+}
+
+function onYouTubeIframeAPIReady() {
+    const videoBlock = $('#player');
+    let videoId = videoBlock.data('video-id');
+    let videoHeight = videoBlock.height()
+    let videoWidth = videoBlock.width();
+
+    console.log(videoId)
+
+    player = new YT.Player('player', {
+        height: videoHeight,
+        width: videoWidth,
+        videoId: videoId,
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+    }
+}
+function stopVideo() {
+    player.stopVideo();
+}
+
+
 
