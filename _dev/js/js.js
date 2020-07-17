f77d8b5fd6a2c3cbad32607649ba917c152cd629
VPayonk <Qwerty123!>
2020-07-17 12:36:19 +0300

    9comm

--- _dev/js/js.js
+++ _dev/js/js.js
@@ -1,3 +1,18 @@
+let foopicker = new FooPicker({
+    id: 'datepicker',
+    dateFormat: 'dd/MM/yyyy'
+});
+
+$('.btn-date').on('click', function () {
+    let datepicker = $('#datepicker');
+    let date = new Date()
+    let day = date.getDate()
+    let month = date.getMonth() < 9 ? `0${date.getMonth() + 1}` : date.getMonth() + 1
+    let year = date.getFullYear()
+
+    datepicker.val(`${day}/${month}/${year}`)
+})
+
 
 $('.play-btn').on('click', function() {
     initVideo()
@@ -53,6 +68,8 @@ function onPlayerStateChange(event) {
 function stopVideo() {
     player.stopVideo();
 }
+
+
 
 
 
